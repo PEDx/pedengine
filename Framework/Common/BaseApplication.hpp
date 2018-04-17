730f110cb7996e5fd4fe45a7690bf64b01254181
ped <pengdong5566@163.com>
2018-04-18 01:16:02 +0800

    添加 cmake 文件

--- Framework/Common/BaseApplication.hpp
+++ Framework/Common/BaseApplication.hpp
@@ -9,5 +9,8 @@ class BaseApplication : implements IApplication {
     virtual void Finalize();
     virtual void Tick();
     virtual bool IsQuit();
+
+  protected:
+    bool m_bQuit;
 };
 } // namespace My
--- Framework/Common/main.cpp
+++ Framework/Common/main.cpp
@@ -1,7 +1,25 @@
+#include "IApplication.hpp"
 #include <stdio.h>
 
-int main() {
-    int a = 1;
-    printf( "Hello Engine%d!\n", a );
+using namespace My;
+
+namespace My {
+extern IApplication *g_pApp;
+}
+
+int main( int argc, char **argv ) {
+    int ret;
+
+    if ( ( ret = g_pApp->Initialize() ) == 0 ) {
+        printf( "Initialize failed!" );
+        return ret;
+    }
+    
+    while ( !g_pApp->IsQuit() ) {
+        g_pApp->Tick();
+    }
+
+    g_pApp->Finalize();
+
     return 0;
 }
